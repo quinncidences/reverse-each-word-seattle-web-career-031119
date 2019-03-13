5f94967a404c720c70710dfd5d572da44ae8f500
Quinn Cox <qrc.inpa@gmail.com>
2019-03-13 15:33:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,10 @@
 def reverse_each_word(sentence)
-  array = []
-  array << sentence.split(" ")
+  array = sentence.split(" ")
   new_array = []
   array.collect do |word| new_array << word.reverse
 end
   new_array.join(" ")
 end
-
-
-
 
 
 
