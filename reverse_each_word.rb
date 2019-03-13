ab2b39c0259e50cf7a0375d8ab6936b1a309a79d
Quinn Cox <qrc.inpa@gmail.com>
2019-03-13 15:30:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,3 @@
-def reverse_each_word(string)
-  array = string.split(" ")
-  new_array = []
-  array.collect do |string|
-    new_array << string.reverse
-end
-  new_array.join(" ")
-end
-
 def reverse_each_word(sentence)
   array = []
   array << sentence.split(" ")
